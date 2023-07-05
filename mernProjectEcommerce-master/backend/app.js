56d148ccd6251c238625f3f428b759f0fd7c3b95
Gauri <2023rsgauri@gmail.com>
2023-07-05 10:08:09 +0530

    first commit

---
+++ mernProjectEcommerce-master/backend/app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const errorMiddleware = require("./middleware/error");
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Route Imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+
+const mongoose = require("mongoose");
+
+// Provide a valid MongoDB connection URI
+const uri = "mongodb://localhost:27017/mydatabase";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    // Connection successful
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    // Connection error
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+
+module.exports = app;
---
+++ mernProjectEcommerce-master/backend/server.js
@@ -0,0 +1,53 @@
+// const app = require("./app");
+// const cloudinary = require("cloudinary");
+// const connectDatabase = require("./config/database");
+
+// // Handling Uncaught Exception
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Uncaught Exception`);
+//   process.exit(1);
+// });
+
+// // Config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
+
+// // Connecting to database
+// connectDatabase();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+// });
+
+// // Unhandled Promise Rejection
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+const mongoose = require("mongoose");
+
+// Provide a valid MongoDB connection URI
+const uri = "mongodb://localhost:27017/mydatabase";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    // Connection successful
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    // Connection error
+    console.error("Error connecting to MongoDB:", error);
+  });
